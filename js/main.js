b2dec64ff02abb6ca5bceaf82334b7d268d58436
andy19910102 <andy19910102@gmail.com>
2016-12-15 19:30:10 +0800

    Revised view

--- js/main.js
+++ js/main.js
@@ -34,6 +34,17 @@ var app = new Vue({
         cvcNumber: "",
         sentTime: ""
     },
+    methods: {
+        isNumber: function (evt) {
+            evt = (evt) ? evt : window.event;
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+                evt.preventDefault();;
+            } else {
+                return true;
+            }
+        }
+    },
     computed: {
         numberGroup1: function () {
             if (this.inputNumbers.substr(0, 4) == '') {
